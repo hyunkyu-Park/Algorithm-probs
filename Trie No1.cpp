283c5f9895a64f8ff2d1d57845c3d8cfcc60a332
hyun-kyu, Park <68415173+hyunkyu-Park@users.noreply.github.com>
2023-08-12 15:01:01 +0900

    Update Trie No1.cpp

--- Trie No1.cpp
+++ Trie No1.cpp
@@ -3,7 +3,15 @@
 #include<cstring>
 #include<vector>
 
+
 using namespace std;
+string arr;
+char ans[26];
+bool visited[26];
+int num_order =0;
+int order = 0;
+char real_ans[26];
+bool find_ans;
 
 class Trie{
     static constexpr size_t M = 26; //the number of English alphabet characters, which is 26
@@ -54,15 +62,48 @@ class Trie{
 		nodes[node_id].is_terminal = false;
 	}
 
-	bool find(const std::string& str) const {
-		int node_id = 0;
-		for (const auto& c : str) {
-			if (nodes[node_id].child[c - OFFSET] == -1) {
-				return false;
-			}
-			node_id = nodes[node_id].child[c - OFFSET];
+	// bool find(const std::string& str) const {
+	// 	int node_id = 0;
+	// 	for (const auto& c : str) {
+	// 		if (nodes[node_id].child[c - OFFSET] == -1) {
+	// 			return false;
+	// 		}
+	// 		node_id = nodes[node_id].child[c - OFFSET];
+	// 	}
+	// 	return nodes[node_id].is_terminal;
+	// }
+
+	void find(int index, int vertex){
+
+		if(find_ans){
+			return;
 		}
-		return nodes[node_id].is_terminal;
+		visited[vertex] = true;
+		
+		if(nodes[vertex].is_terminal){
+			order++;
+			if(order == index){
+				find_ans = true;
+				for(int i=0; i<num_order; ++i){
+					real_ans[i] = ans[i];
+				}
+				
+				return;
+			}
+		}
+
+		
+		for(int i=0; i<26; ++i){
+			if(nodes[vertex].child[i] != -1 && !visited[nodes[vertex].child[i]]){
+				ans[num_order++] = 'a' + i;
+				find(index, nodes[vertex].child[i]);
+				num_order--;
+			}
+			auto check = nodes[vertex].child[i];
+			auto check1 = visited[nodes[vertex].child[i]];
+			
+		}
+		
 	}
 };
 
@@ -73,12 +114,21 @@ int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
-	string arr;
+	
 
 	cin>>T;
 
 	for(test_case = 1; test_case <= T; ++test_case)
 	{
+		memset(ans, 0, sizeof(ans));
+		memset(visited, false, sizeof(visited));
+		memset(real_ans, 0, sizeof(real_ans));
+		order =0;
+		num_order =0;
+		
+		int index;
+		cin >> index;
+
 		cin >> arr;
 		
 		Trie root;
@@ -86,14 +136,17 @@ int main(int argc, char** argv)
 		for (int i = 0; i < arr.length(); i++) {
 			root.insert(arr.substr(i));
 		}
+
+		
 		//If you search for dfs in reverse order(26-0), it comes in dictionary order after insertion
-		//다 삽입하고 역순(26~0)으로 dfs 검색하면 사전순으로 나온다
-		bool check1 = root.find("abac");
-		bool check2 = root.find("bac");
-		bool check3 = root.find("ac");
-		bool check4 = root.find("c");
-		bool check5 = root.find("ca");
-		cout <<1;
+		//다 삽입하고 dfs 검색하면 사전순으로 나온다
+		root.find(index, 0);
+		cout << "#" << test_case <<" " << real_ans << "\n";
+
+		
+		
+		
+		
 
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
