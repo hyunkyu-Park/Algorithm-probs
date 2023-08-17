2e8c40da39f55c8605ceedde38b9fee080c7ad14
hyun-kyu, Park <68415173+hyunkyu-Park@users.noreply.github.com>
2023-08-17 14:48:52 +0900

    Update news-simulation,hash.h

--- news-simulation,hash.h
+++ news-simulation,hash.h
@@ -7,29 +7,31 @@
 
 using namespace std;
 
-typedef struct NEWS
-{
+struct NEWS{
 	int release_time;
 	int news_id;
 	int channel_id;
 	bool canceled;
 
-    bool operator>(const NEWS& other) const{
-        if(release_time == other.release_time){
-            return news_id > other.news_id;
-        }
-        return release_time > other.release_time;
-    }
-}NEWS;
+	bool operator() (const NEWS* a, const NEWS* b) const
+	{
+		if (a->release_time == b->release_time)
+			return a->news_id > b->news_id;
+		return a->release_time > b->release_time;
+	}
+};
 
-unordered_map<int, int> user_info; //UID를 user_pool에서 사용하는 idx로 변한하기 위한 해쉬
-unordered_map<int, int> news_info;
-unordered_map<int, int> channel_info;
-priority_queue<NEWS*, vector<NEWS*>, NEWS> news_queue;
 
+
+unordered_map<int, int> user_info; //Hash to change uID to idx for user_pool
+unordered_map<int, int> news_info; //Hash to change NewsID to idx for news_pool
+unordered_map<int, int> channel_info; //Hash to change ChannelID to idx for channel_pool
+priority_queue<NEWS*, vector<NEWS*>, NEWS> news_queue; //PQ that stores NEWS pointer
+
+//NEWS are only stored in news_pool
 NEWS news_pool[NEWS_SIZE]; int npi;
-vector<NEWS*> user_pool[USER_SIZE]; int upi;
-vector<int> channel_pool[CHANNEL_SIZE]; int cpi; //각 채널별 구독중인 uID를 저장
+vector<NEWS*> user_pool[USER_SIZE]; int upi; //Stores NEWS announced to user
+vector<int> channel_pool[CHANNEL_SIZE]; int cpi; //Stores registered uID for each channel
 
 void init(int N, int K)
 {
@@ -41,20 +43,21 @@ void init(int N, int K)
 	upi = npi = cpi = 0;
 }
 
-void updateNews(int mTime) //mTime 시각에 유저에게 보내지는 뉴스 알림이 있는 경우 먼저 알림을 보내기 위한 함수
+
+void updateNews(int mTime) //send a notification first if there is a news sent to the user at the mTime
 {
 	while (!news_queue.empty())
 	{
 		auto cur = news_queue.top();
-		if (cur->release_time > mTime) //만약 현재 시간보다 release_time이 크다면 업데이트 x
+		if (cur->release_time > mTime) //If release_time is later than mTime, nothing happens
 			return;
 		news_queue.pop();
 
-		if (now_news->canceled) //만약 취소된 뉴스라면
+		if (cur->canceled) //If its canceled NEWS
 			continue;
 
-		for (int i = 0; i < channel_pool[now_news->channel_id].size(); i++) //해당 채널을 구독중인 유저들에게 뉴스 알람을 보냄
-			user_pool[channel_pool[now_news->channel_id][i]].push_back(now_news);
+		for (int i = 0; i < channel_pool[cur->channel_id].size(); i++) //send NEWS to the registered users
+			user_pool[channel_pool[cur->channel_id][i]].push_back(cur);
 	}
 }
 
@@ -62,61 +65,76 @@ void registerUser(int mTime, int mUID, int mNum, int mChannelIDs[])
 {
 	updateNews(mTime);
 
-	int now_uId; //원래 UID가 아니라 user_pool에서 사용하는 idx
-	if (user_info.find(mUID) == user_info.end()) //해당 유저가 해쉬에 없다면(처음 등장한 유저라면)
+	int now_uId; //not user's ID, but idx idx for user_pool
+	if (user_info.find(mUID) == user_info.end()) //If mUID is not in the hash(=New User)
 	{
-		now_uId = user_info[mUID] = upi++; //user_info에 등록
+		now_uId = user_info[mUID] = upi++; //Add to user_info
 		user_pool[now_uId].clear();
 	}
 	else
 		now_uId = user_info[mUID];
 
-	for (int i = 0; i < mNum; i++) //유저가 등록하는 모든 채널에 유저를 추가
+	for (int i = 0; i < mNum; i++) //Add uID to mChannelIDs[]
 	{
-		int now_cId; //원래 ChannelID가 아니라 channel_info에서 사용하는 Idx
-		if (channel_info.find(mChannelIDs[i]) == channel_info.end()) //해당 채널이 해쉬에 없다면(처음 등장한 채널이라면)
+		int now_cId; //Not just ChannelID, but idx for channel_info
+		if (channel_info.find(mChannelIDs[i]) == channel_info.end()) //If the channel is not in the hash(=New channel)
 		{
-			now_cId = channel_info[mChannelIDs[i]] = cpi++; //channel_info에 등록
+			now_cId = channel_info[mChannelIDs[i]] = cpi++; //Add to channel_info
 			channel_pool[now_cId].clear();
 		}
 		else
 			now_cId = channel_info[mChannelIDs[i]];
-
-		channel_pool[now_cId].push_back(now_uId); //channel_pool에, 해당 채널에 맞는 idx에 user_pool에서 사용하는 idx 저장
+												
+		channel_pool[now_cId].push_back(now_uId); //save idx for user_pool in the channel_pool
 	}
 }
 
+// int release_time;
+// int news_id;
+// int channel_id;
+// bool canceled;
 int offerNews(int mTime, int mNewsID, int mDelay, int mChannelID)
 {
-	// int now_cId = channel_info[mChannelID]; //
-	// int now_nId = news_info[mNewsID] = npi++;
-	// news_pool[now_nId] = { mTime + mDelay, mNewsID, now_cId, false };
+	news_pool[npi] = {mTime+mDelay, mNewsID, channel_info[mChannelID], false};
+	//Since PQ stores address of the NEWS in news_pool, 
+	//when value in the news_pool is changed the value in the PQ is also changed
+	news_queue.push(&news_pool[npi]); 
+	news_info[mNewsID] = npi++;
 
-	// news_queue.push(&news_pool[now_nId]);
+	return channel_pool[channel_info[mChannelID]].size();
 
-	// return channel_pool[now_cId].size();
 }
 
 void cancelNews(int mTime, int mNewsID)
 {
-	// news_pool[news_info[mNewsID]].canceled = true;
+	news_pool[news_info[mNewsID]].canceled = true;
 }
 
 int checkUser(int mTime, int mUID, int mRetIDs[])
 {
-	// updateNews(mTime);
+	updateNews(mTime);
 
-	// int now_uId = user_info[mUID];
-	// int ret = user_pool[now_uId].size();
-	// int cnt = 0;
-	// for (int i = user_pool[now_uId].size() - 1; i >= 0; i--)
-	// {
-	// 	if (user_pool[now_uId][i]->canceled)
-	// 		ret--;
-	// 	else if (cnt < 3)
-	// 		mRetIDs[cnt++] = user_pool[now_uId][i]->news_id;
-	// }
-	// user_pool[now_uId].clear();
+	int user_idx = user_info[mUID];
+	int ans = user_pool[user_idx].size();
+	int cnt = 0;
 
-	// return ret;
+	//News is stored in chronological order
+	//It means the latest NEWS is stored at the very back
+	//Check from the back
+	for(int i=user_pool[user_idx].size()-1; i>= 0; --i){
+		if(user_pool[user_idx][i]->canceled){
+			ans--;
+		}
+		else if(cnt < 3){
+			mRetIDs[cnt++] = user_pool[user_idx][i]->news_id;
+		}
+	}
+
+	//After the function is called, 
+	//all news notifications received by the mUID user are deleted, 
+	//and the number of notifications becomes 0.
+	user_pool[user_idx].clear();
+
+	return ans;
+
 }
